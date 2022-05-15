19fa0a39e9631aa5606ae8cbb55b7591093aa9de
Sam Stanier <sas229@cam.ac.uk>
2022-05-15 13:36:09 +0100

    Trying an alternative icon strategy for Windows.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os, sys
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
+from PySide6.QtCore import QSize
 from log import init_log
 from widgets.MainWindow import MainWindow
 import logging
@@ -9,18 +10,27 @@ if __name__ == '__main__':
     # Create log file instance.
     init_log()
     log = logging.getLogger(__name__)
-    log.info('Log file created.')
-
-    # Get directory.
-    bundle_dir = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
-    path_to_icon = os.path.abspath(os.path.join(bundle_dir,"assets/NRFIS.ico"))
+    log.info('Log file created.')    
 
     # Generate application instance.
     app = QApplication(sys.argv)
     app.setOrganizationName("CUED")
     app.setOrganizationDomain("Civil")
     app.setApplicationName("CamLab")
-    app.setWindowIcon(QIcon(path_to_icon))
+    
+    # Get directory.
+    bundle_dir = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
+    icon_dir = os.path.abspath(os.path.join(bundle_dir,"assets"))   
+
+    # Set app icon.   
+    app_icon = QIcon()
+    app_icon.addFile(os.path.abspath(os.path.join(icon_dir,"CamLab_16_16.png")), QSize(16,16))
+    app_icon.addFile(os.path.abspath(os.path.join(icon_dir,"CamLab_24_24.png")), QSize(24,24))
+    app_icon.addFile(os.path.abspath(os.path.join(icon_dir,"CamLab_32_32.png")), QSize(32,32))
+    app_icon.addFile(os.path.abspath(os.path.join(icon_dir,"CamLab_48_48.png")), QSize(48,48))
+    app_icon.addFile(os.path.abspath(os.path.join(icon_dir,"CamLab_256_256.png")), QSize(256,256))
+    app_icon.addFile(os.path.abspath(os.path.join(icon_dir,"CamLab.png")), QSize(683,683))
+    app.setWindowIcon(app_icon)
 
     # Execute main window.
     main = MainWindow()
