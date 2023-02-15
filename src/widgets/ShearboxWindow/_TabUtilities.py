c471b16c81474d788e37fc82cfd8cc7edfa73eb3
Scott Irvine <scotti2001@yahoo.co.uk>
2023-02-15 11:31:05 +0000

    add code for channel selection

--- src/widgets/ShearboxWindow/_TabUtilities.py
+++ src/widgets/ShearboxWindow/_TabUtilities.py
@@ -15,22 +15,22 @@ class TabUtilities:
             self.devices[key] = [device, channels]
         # print(self.devices.keys())
         self.tabs.horiz_load_ins.clear()
-        self.tabs.horiz_load_ins.addItems([""] + list(self.devices.keys()))
+        self.tabs.horiz_load_ins.addItems([None] + list(self.devices.keys()))
         self.tabs.horiz_load_chan.clear()
         self.tabs.horiz_disp_ins.clear()
-        self.tabs.horiz_disp_ins.addItems([""] + list(self.devices.keys()))
+        self.tabs.horiz_disp_ins.addItems([None] + list(self.devices.keys()))
         self.tabs.horiz_disp_chan.clear()
         self.tabs.vert_load_ins.clear()
-        self.tabs.vert_load_ins.addItems([""] + list(self.devices.keys()))
+        self.tabs.vert_load_ins.addItems([None] + list(self.devices.keys()))
         self.tabs.vert_load_chan.clear()
         self.tabs.vert_disp_ins.clear()
-        self.tabs.vert_disp_ins.addItems([""] + list(self.devices.keys()))
+        self.tabs.vert_disp_ins.addItems([None] + list(self.devices.keys()))
         self.tabs.vert_disp_chan.clear()
         self.tabs.horiz_cont_ins.clear()
-        self.tabs.horiz_cont_ins.addItems([""] + list(self.devices.keys()))
+        self.tabs.horiz_cont_ins.addItems([None] + list(self.devices.keys()))
         self.tabs.horiz_cont_chan.clear()
         self.tabs.vert_cont_ins.clear()
-        self.tabs.vert_cont_ins.addItems([""] + list(self.devices.keys()))
+        self.tabs.vert_cont_ins.addItems([None] + list(self.devices.keys()))
         self.tabs.vert_cont_chan.clear()
 
     def set_horiz_load_ins(self, device):
@@ -38,7 +38,7 @@ class TabUtilities:
             self.configuration["shearbox"]["horiz_load_ins"] = self.devices[device][0]
             log.info(f"Device selected: {device}")
             self.tabs.horiz_load_chan.clear()
-            self.tabs.horiz_load_chan.addItems([""] + self.devices[device][1])
+            self.tabs.horiz_load_chan.addItems([None] + self.devices[device][1])
         else:
             log.info("No device selected")
             self.configuration["shearbox"]["horiz_load_ins"] = None
@@ -46,14 +46,20 @@ class TabUtilities:
         self.update_configuration()
 
     def set_horiz_load_chan(self, channel):
-        pass
+        if channel != "":
+            self.configuration["shearbox"]["horiz_load_chan"] = channel
+            log.info(f"Channel selected: {channel}")
+        else:
+            log.info("No device selected")
+            self.configuration["shearbox"]["horiz_load_chan"] = None
+        self.update_configuration()
 
     def set_horiz_disp_ins(self, device):
         if device != "":
             self.configuration["shearbox"]["horiz_disp_ins"] = self.devices[device][0]
             log.info(f"Device selected: {device}")
             self.tabs.horiz_disp_chan.clear()
-            self.tabs.horiz_disp_chan.addItems([""] + self.devices[device][1])
+            self.tabs.horiz_disp_chan.addItems([None] + self.devices[device][1])
         else:
             log.info("No device selected")
             self.configuration["shearbox"]["horiz_disp_ins"] = None
@@ -61,68 +67,94 @@ class TabUtilities:
         self.update_configuration()
 
     def set_horiz_disp_chan(self, channel):
-        pass
+        if channel != "":
+            self.configuration["shearbox"]["horiz_load_chan"] = channel
+            log.info(f"Channel selected: {channel}")
+        else:
+            log.info("No device selected")
+            self.configuration["shearbox"]["horiz_load_chan"] = None
+        self.update_configuration()
 
     def set_vert_load_ins(self, device):
         if device != "":
             self.configuration["shearbox"]["vert_load_ins"] = self.devices[device][0]
             log.info(f"Device selected: {device}")
             self.tabs.vert_load_chan.clear()
-            self.tabs.vert_load_chan.addItems([""] + self.devices[device][1])
+            self.tabs.vert_load_chan.addItems([None] + self.devices[device][1])
         else:
             log.info("No device selected")
             self.configuration["shearbox"]["vert_load_ins"] = None
             self.tabs.vert_load_chan.clear()
         self.update_configuration()
-        pass
 
     def set_vert_load_chan(self, channel):
-        pass
+        if channel != "":
+            self.configuration["shearbox"]["horiz_load_chan"] = channel
+            log.info(f"Channel selected: {channel}")
+        else:
+            log.info("No device selected")
+            self.configuration["shearbox"]["horiz_load_chan"] = None
+        self.update_configuration()
 
     def set_vert_disp_ins(self, device):
         if device != "":
             self.configuration["shearbox"]["vert_disp_ins"] = self.devices[device][0]
             log.info(f"Device selected: {device}")
             self.tabs.vert_disp_chan.clear()
-            self.tabs.vert_disp_chan.addItems([""] + self.devices[device][1])
+            self.tabs.vert_disp_chan.addItems([None] + self.devices[device][1])
         else:
             log.info("No device selected")
             self.configuration["shearbox"]["vert_disp_ins"] = None
             self.tabs.vert_disp_chan.clear()
         self.update_configuration()
-        pass
 
     def set_vert_disp_chan(self, channel):
-        pass
+        if channel != "":
+            self.configuration["shearbox"]["horiz_load_chan"] = channel
+            log.info(f"Channel selected: {channel}")
+        else:
+            log.info("No device selected")
+            self.configuration["shearbox"]["horiz_load_chan"] = None
+        self.update_configuration()
 
     def set_horiz_cont_ins(self, device):
         if device != "":
             self.configuration["shearbox"]["horiz_cont_ins"] = self.devices[device][0]
             log.info(f"Device selected: {device}")
             self.tabs.horiz_cont_chan.clear()
-            self.tabs.horiz_cont_chan.addItems([""] + self.devices[device][1])
+            self.tabs.horiz_cont_chan.addItems([None] + self.devices[device][1])
         else:
             log.info("No device selected")
             self.configuration["shearbox"]["horiz_cont_ins"] = None
             self.tabs.horiz_cont_chan.clear()
         self.update_configuration()
-        pass
 
     def set_horiz_cont_chan(self, channel):
-        pass
+        if channel != "":
+            self.configuration["shearbox"]["horiz_load_chan"] = channel
+            log.info(f"Channel selected: {channel}")
+        else:
+            log.info("No device selected")
+            self.configuration["shearbox"]["horiz_load_chan"] = None
+        self.update_configuration()
 
     def set_vert_cont_ins(self, device):
         if device != "":
             self.configuration["shearbox"]["vert_cont_ins"] = self.devices[device][0]
             log.info(f"Device selected: {device}")
             self.tabs.vert_cont_chan.clear()
-            self.tabs.vert_cont_chan.addItems([""] + self.devices[device][1])
+            self.tabs.vert_cont_chan.addItems([None] + self.devices[device][1])
         else:
             log.info("No device selected")
             self.configuration["shearbox"]["vert_cont_ins"] = None
             self.tabs.vert_cont_chan.clear()
         self.update_configuration()
-        pass
 
     def set_vert_cont_chan(self, channel):
-        pass
+        if channel != "":
+            self.configuration["shearbox"]["horiz_load_chan"] = channel
+            log.info(f"Channel selected: {channel}")
+        else:
+            log.info("No device selected")
+            self.configuration["shearbox"]["horiz_load_chan"] = None
+        self.update_configuration()
